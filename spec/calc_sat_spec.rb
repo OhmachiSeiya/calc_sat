a06315aaf33a8f7c6fa56c9591012bc4be22633d
OhmachiSeiya <OhmachiSeiya@gmail.com>
2021-05-23 07:27:25 +0900

    initial commit

---
+++ spec/calc_sat_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe CalcSat do
+  it "has a version number" do
+    expect(CalcSat::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
